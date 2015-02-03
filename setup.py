3f85d45cfb6f234ca629010d71a098c39c0ec075
Pont Lurcock <pont@talvi.net>
2015-02-03 22:48:19 +0100

    Inital commit
    Library code as used for producing plots and analyses in thesis.
    As such, functional but overly specialized and poorly documented.

---
+++ setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(name='tdms',
+      version='0.0',
+      description='Manipulation of temperature-dependent magnetic susceptibility data',
+      url='http://fixme',
+      author='Pontus Lurcock',
+      author_email='pont@talvi.net',
+      classifiers=['Development Status :: 4 - Beta',
+                   'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+                   'Topic :: Scientific/Engineering',
+                   'Programming Language :: Python',
+                   'Intended Audience :: Science/Research`'
+               ],
+      packages=['tdms'],
+      zip_safe=False)
---
+++ tdms/__init__.py
@@ -0,0 +1,1 @@
+all = ["tdms_lib"]
