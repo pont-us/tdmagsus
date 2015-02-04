26566717b36bc91753f60df5a2b576c5705898cc
Pont Lurcock <pont@talvi.net>
2015-02-04 14:49:53 +0100

    Deal with single-cycle measurements.
    Existing code assumed a series of heating cycles on the same sample.
    Created a MeasurementCycle class which can be used to correct data
    for a single measurement cycle. More refactoring yet to do but the
    current code works as is for simple cases.

--- tdms/tdms_lib.py
+++ tdms/tdms_lib.py
@@ -12,6 +12,19 @@ field_separator = re.compile(' +')
 # TODO: fix splines at endpoints
 
 def read_cur_file(filename):
+    """Read a .CUR magnetic susceptibility file
+
+    Args:
+      filename (str): name of file to read
+
+    Returns:
+      ([heating_temps, heating_mag_sus_values],
+       [cooling_temps, cooling_mag_sus_values])
+
+      This is a tuple of two lists, each containing
+      two numpy arrays.
+    """
+
     infile = open(filename, 'r')
     heating = ([],[])
     cooling = ([],[])
@@ -33,7 +46,7 @@ def read_cur_file(filename):
     cooling[1].reverse()
     heating = (heating[0][1:], heating[1][1:],)
     cooling = (cooling[0][1:], cooling[1][1:],)
-    return (map(array,heating), map(array,cooling))
+    return (map(array, heating), map(array, cooling))
     
 class Furnace:
 
@@ -65,7 +78,28 @@ class Furnace:
         return (Furnace.correct_with_spline(heating, self.heat_spline),
                 Furnace.correct_with_spline(cooling, self.cool_spline))
 
-class TdmsData:
+class MeasurementCycle:
+    """The results of a single heating-cooling run."""
+
+    def __init__(self, furnace, filename, real_vol, nom_vol):
+        self.furnace = furnace
+        self.real_vol = real_vol
+        self.nom_vol = nom_vol
+        (heating, cooling) = read_cur_file(filename)
+        if self.furnace != None:
+            heating, cooling = self.furnace.correct(heating, cooling)
+        #heating = (heating[0], TdmsData.shunt_up(heating[1]))
+        #cooling = (cooling[0], TdmsData.shunt_up(cooling[1]))
+        heating = (heating[0], self.correct_for_volume(heating[1]))
+        cooling = (cooling[0], self.correct_for_volume(cooling[1]))
+        self.data = (heating, cooling)
+
+    def correct_for_volume(self, data):
+        scale = self.nom_vol / self.real_vol
+        return [scale * datum for datum in data]
+
+class MeasurementRunSet:
+    """The results of a series of heating-cooling runs on the same sample."""
 
     @staticmethod
     def shunt_up(values):
@@ -83,12 +117,12 @@ class TdmsData:
 
     def make_zero_at_700(self):
         'Correct values for a zero susceptibility at/near 700 degrees'
-        print self.name, self.data.keys(), self.data[700][0][1][:5]
-        offset = -min(self.data[700][0][1][-5:])
+        print self.name, self.cycles.keys(), self.cycles[700][0][1][:5]
+        offset = -min(self.cycles[700][0][1][-5:])
         new_data = {}
-        for temp in self.data.keys():
-            new_data[temp] = TdmsData.shunt(self.data[temp], offset)
-        self.data = new_data
+        for temp in self.cycles.keys():
+            new_data[temp] = TdmsData.shunt(self.cycles[temp], offset)
+        self.cycles = new_data
 
     @staticmethod
     def filename_to_temp(filename):
@@ -98,18 +132,14 @@ class TdmsData:
         if m==None: return None
         return int(m.group(1))
 
-    def correct_for_volume(self, data):
-        scale = self.nom_vol / self.real_vol
-        return [scale * datum for datum in data]
-
     def set_oom(self, new_oom):
         scale = 10. **(self.oom - new_oom)
         new_data = {}
-        for (temp, (heating, cooling)) in self.data.items():
+        for (temp, (heating, cooling)) in self.cycles.items():
             heating2 = (heating[0], [ms*scale for ms in heating[1]])
             cooling2 = (cooling[0], [ms*scale for ms in cooling[1]])
             new_data[temp] = (heating2, cooling2)
-        self.data = new_data
+        self.cycles = new_data
         self.oom = new_oom
 
     def read_files(self, sample_dir):
@@ -117,23 +147,14 @@ class TdmsData:
         for filename in cur_files:
             temperature = TdmsData.filename_to_temp(filename)
             if temperature == None: continue
-            (heating, cooling) = read_cur_file(filename)
-            if self.furnace != None:
-                heating, cooling = self.furnace.correct(heating, cooling)
-            #heating = (heating[0], TdmsData.shunt_up(heating[1]))
-            #cooling = (cooling[0], TdmsData.shunt_up(cooling[1]))
-            heating = (heating[0], self.correct_for_volume(heating[1]))
-            cooling = (cooling[0], self.correct_for_volume(cooling[1]))
-            self.data[temperature] = (heating, cooling)
+            self.cycles[temperature] = MeasurementCycle(self.furnace, filename, self.real_vol, self.nom_vol)
         # self.make_zero_at_700()
-        self.samples.sort()
 
     def __init__(self, furnace, sample_dir, real_vol=0.25, nom_vol = 10.):
         self.oom = -6. # order of magnitude
         self.name = os.path.basename(sample_dir)
         self.furnace = furnace
-        self.data = {}
-        self.samples = []
+        self.cycles = {}
         self.nom_vol = nom_vol
         self.real_vol = real_vol
         if (sample_dir != None): self.read_files(sample_dir)
@@ -163,7 +184,7 @@ class TdmsData:
     def curie_paramag(self, cycle, min_temp, max_temp):
         ''' Estimates a Curie temperature by linear fit to 
         inverse susceptibility over a given range.'''
-        (temps, mss) = TdmsData.chop_data(self.data[cycle][1], min_temp, max_temp)
+        (temps, mss) = TdmsData.chop_data(self.cycles[cycle][1], min_temp, max_temp)
         poly, rsquared = TdmsData.linear_fit(temps, 1./mss)
         curie = poly.r[0] # x axis intercept
         return (curie, rsquared, poly)
@@ -171,7 +192,7 @@ class TdmsData:
     def curie_inflection(self, cycle, min_temp, max_temp):
         ''' Estimates a Curie temperature by finding an inflection
         point on a Hopkinson peak.'''
-        all_temps, all_mss = self.data[cycle][1]
+        all_temps, all_mss = self.cycles[cycle][1]
         (temps, mss) = TdmsData.chop_data((all_temps, all_mss), min_temp, max_temp)
         spline = UnivariateSpline(all_temps, all_mss, s=.1)
         derivs = [ spline.derivatives(t)[2] for t in temps ]
@@ -179,7 +200,7 @@ class TdmsData:
         return (spline2.roots()[0], spline)
 
     def alteration(self, cycle):
-        return self.data[cycle][1][1][0] -  self.data[cycle][0][1][0]
+        return self.cycles[cycle][1][1][0] -  self.cycles[cycle][0][1][0]
 
     def alterations(self, cycles):
         return [self.alteration(cycle) for cycle in cycles]
