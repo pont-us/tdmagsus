a86129fe2aa7ed6e00ecc065c5d8a861402ad1ad
Pont Lurcock <pont@talvi.net>
2015-02-05 14:38:54 +0100

    Update setup file; add readme.

---
+++ README.rst
@@ -0,0 +1,5 @@
+tdmagsus: tools for thermomagnetic susceptibility analysis
+==========================================================
+
+This is a small library for working with temperature-dependent
+magnetic susceptibility data measured on an AGICO kappabridge.
--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from setuptools import setup
 
-setup(name='tdms',
-      version='0.0',
-      description='Manipulation of temperature-dependent magnetic susceptibility data',
-      url='http://fixme',
-      author='Pontus Lurcock',
-      author_email='pont@talvi.net',
-      classifiers=['Development Status :: 4 - Beta',
-                   'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
-                   'Topic :: Scientific/Engineering',
-                   'Programming Language :: Python',
-                   'Intended Audience :: Science/Research`'
-               ],
-      packages=['tdms'],
-      zip_safe=False)
+setup(name = "tdmagsus",
+      version = "0.0",
+      description =
+      "Manipulation of temperature-dependent "
+      "magnetic susceptibility data",
+      url = "https://github.com/pontu/tdmagsus",
+      author = "Pontus Lurcock",
+      author_email = "pont@talvi.net",
+      license = "GNU GPLv3+",
+      classifiers = ["Development Status :: 4 - Beta",
+                     "License :: OSI Approved :: "
+                     "GNU General Public License v3 or later (GPLv3+)",
+                     "Topic :: Scientific/Engineering",
+                     "Programming Language :: Python",
+                     "Intended Audience :: Science/Research`"
+                 ],
+      packages = ["tdmagsus"],
+      install_requires = ["numpy", "scipy"],
+      zip_safe = False)
