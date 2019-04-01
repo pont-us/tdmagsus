bf005ee386d3198d1027bd0e93db599c6a0c7c4e
Pontus Lurcock <pont@talvi.net>
2019-04-01 15:16:21 +0200

    Update version number and classifiers in setup.py

--- setup.py
+++ setup.py
@@ -3,19 +3,19 @@
 from setuptools import setup
 
 setup(name="tdmagsus",
-      version="0.0",
+      version="1.0",
       description=
       "Manipulation of temperature-dependent magnetic susceptibility data",
       url="https://github.com/pont-us/tdmagsus",
       author="Pontus Lurcock",
       author_email="pont@talvi.net",
       license="GNU GPLv3+",
-      classifiers=["Development Status :: 4 - Beta",
+      classifiers=["Development Status :: 5 - Production/Stable",
                    "License :: OSI Approved :: "
                    "GNU General Public License v3 or later (GPLv3+)",
                    "Topic :: Scientific/Engineering",
-                   "Programming Language :: Python",
-                   "Intended Audience :: Science/Research`"
+                   "Programming Language :: Python :: 3",
+                   "Intended Audience :: Science/Research"
                    ],
       packages=["tdmagsus"],
       install_requires=["numpy", "scipy"],
