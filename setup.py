b2d92ffecd9c0205b8e091763aba6d9d810da479
Pontus Lurcock <pont@talvi.net>
2019-04-22 16:13:00 +0200

    Update version number and add long description
     - Change version number from 1.0 to 1.0.0 (semantic versioning).
    
     - Add a long description (pulled from the readme) to setup.py.

--- setup.py
+++ setup.py
@@ -1,11 +1,19 @@
 #!/usr/bin/env python3
 
+import os.path
 from setuptools import setup
 
+cwd = os.path.abspath(os.path.dirname(__file__))
+
+with open(os.path.join(cwd, "README.rst")) as fh:
+    long_desc = fh.read()
+
 setup(name="tdmagsus",
-      version="1.0",
+      version="1.0.0",
       description=
       "Manipulation of temperature-dependent magnetic susceptibility data",
+      long_description_content_type="text/x-rst",
+      long_description=long_desc,
       url="https://github.com/pont-us/tdmagsus",
       author="Pontus Lurcock",
       author_email="pont@talvi.net",
